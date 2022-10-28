485b799d8b4f8b575b630d7a08224e04ec6f38b1
Anya Hanatuke <anyahanatuke@gmail.com>
2022-10-28 13:54:47 -0600

    pushing to main cuz i broke it and now i fix it

--- main.py
+++ main.py
@@ -23,6 +23,53 @@ def introLoop():
     return
 
 
+def regInputs():
+    inputU = input("Please enter a user id: ")
+    inputN = input("Please enter your name: ")
+    inputP = getpass.getpass(prompt="Enter a password: ")
+    inputP2 = getpass.getpass(prompt="Re-enter password: ")
+    while inputP != inputP2:
+        print("Passwords don't match, please try again")
+        inputP2 = getpass.getpass(prompt="Re-enter password: ")
+
+    return inputU, inputN, inputP, inputP2
+
+
+def register():
+    connection, cursor = connect(path)
+    q = '''SELECT *
+    FROM users as u
+    '''
+    cursor.execute(q)
+    usersAmount = cursor.fetchone()
+    connection.commit()
+
+    usersAmount += 1
+    print("Suggested user u", len(usersAmount), ": ")
+    inputU, inputN, inputP, inputP2 = regInputs()
+
+    reEnter = input("Keep the following [Y/N]?: \n" + inputU + "\n" + inputN + " ")
+    check = FALSE
+
+    while check == FALSE:
+
+        if reEnter.lower() == 'n':
+            inputU, inputN, inputP, inputP2 = regInputs()
+            break
+        elif reEnter.lower() == 'y':
+            q = '''INSERT INTO users 
+            VALUES ((?), (?), (?))'''
+            cursor.execute(q, (inputU, inputN, inputP))
+            connection.commit()
+            break
+        else:
+            print("Invalid input. Please try again")
+            reEnter = input("Keep the following [Y/N]?: \n" + inputU + "\n" + inputN + " ")
+
+    return
+
+
+
 def main():
 
     #link database using URL 
