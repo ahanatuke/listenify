a64ed0f4be758ad33c38d5342115ea26e313f31d
a-jutt <97474113+a-jutt@users.noreply.github.com>
2022-11-01 15:05:16 -0600

    A lot has been done here, please ping me for every q you got Lets get this done yall

--- main.py
+++ main.py
@@ -141,12 +141,10 @@ def register():
     FROM users as u
     '''
     cursor.execute(q)
-    usersAmount = cursor.fetchone()   
+    usersAmount = len(cursor.fetchall()) + 1   
     connection.commit()
 
-    
-    usersAmount += 1
-    print("Suggested user u" , len(usersAmount), ": ")
+    print("Suggested user u" , usersAmount, ": ")
     inputU, inputN, inputP, inputP2 = regInputs()
    
     reEnter = input("Keep the following [Y/N]?: \n"+inputU+"\n"+inputN+" ")
@@ -172,27 +170,322 @@ def register():
  
 
 
-
-def login():
-    return 
-def introLoop():
+def login(cursor):
+    #todo someone look at this and tell me if its okay lmao
+   
     return
 
 
-def main():
 
-    #link database using URL 
+def introLoop():
+    print("Press 'L' to login to an existing account.\nPress 'R' to register a new account.\nPress 'Q' to quit.")
+    userInput = input("> ")
+    userInput = userInput.lower().strip()
+    while userInput != "r" and userInput != "l" and userInput != "q":
+        print("Invalid input. Please try again.")
+        userInput = input("> ")
+        userInput = userInput.lower().strip()
+    return userInput
+
+def addSong():
+    '''CONCEPT: ask for title and duration as inputs
+    get all songs and create a new sid by adding length of all songs + 1
+    from there see if we can select a song from the input provided by the artist 
+    a song provided by the artist should be unique, and therefore if we check the len of songExist we should get a len of 0
+    If the song doesn't exist we insert it, and then request an input about who the features are, once provided do a for loop and add every feature
+    Make sure to confirm that is either added in or exists'''
+    
+    connection, cursor = connect(path)
+   
+   #get all songs and get len 
+    q = '''SELECT *
+            FROM songs as s'''
+    cursor.execute(q)
+    sidNew = len(cursor.fetchall()) + 1
+    connection.commit()
+
+    print("Please enter the title and duration (in seconds): ")
+    title = input("Title: ")
+    duration = input("Duration (in seconds): ")
+    duration = int(duration)
+    
+    #CHECK IF THE SONG EXISTS
+    q = '''SELECT  s.title, s.duration
+        FROM songs as s, artists as a, perform as p
+        WHERE s.title = ? AND s.duration = ? AND a.aid = ? AND p.sid = s.sid AND p.aid = a.aid 
+        '''
+    cursor.execute(q, (title, duration, artist, ))
+    #1 song should exist like this, len of fetchone = 0 it should be unique HYPOTHETICALLY
+    songExist = len(cursor.fetchone())
+    connection.commit()
+
+    if songExist == 0: 
+        q = '''INSERT INTO songs 
+        VALUES(?, ?, ?)'''
+        cursor.execute(q, (sidNew, title, duration,))
+        connection.commit()
+        userInput = input("Please list the aid's of everyone who is featured on your song (hit ENTER if none): ")
+        inputChecker = userInput.split()
+        if inputChecker != '':
+            result = [x.strip() for x in userInput.split(',')]
+            #assuming we dont need to validate aids for every feature 
+            for feature in result:
+                q = '''INSERT INTO perform 
+                VALUES (?, ?)'''
+                cursor.execute(q, (feature, sidNew))
+                connection.commit()
+        print("Song %s has been successfully added in." %songExist)
+            
+        
+    else:
+        print("Cannot add, this song already exists.")
+        
+    return
+
+def topListen():
+    
+    connection, cursor = connect(path)
+    q = '''SELECT u.uid
+    FROM
+    WHERE
+    ORDER BY u.uid DESC
+    LIMIT 3
+    '''
+    cursor.execute(q)
+    top3U = cursor.fetchall()
+    connection.commit()
+    for user in top3U:
+        print(user)
+
+    
+    q = '''SELECT p.pid
+    FROM
+    WHERE
+    ORDER BY p.pid DESC
+    LIMIT 3
+    '''
+    cursor.execute(q)
+    top3P = cursor.fetchall()
+    connection.commit()
+    for playlist in top3P:
+        print(playlist)
+
+def artist(artist):
+    #artist is an aid of the user who logged in, used to check if a song exists or not 
+    connection, cursor = connect(path)
+    print("Enter 'S' to add a song.\nEnter 'F' to find your top listeners and playlists with most of your songs.")
+    userInput = input("> ")
+    userInput = userInput.lower().strip()
+    while userInput != "s" and userInput != "f":
+        print("Invalid input. Please try again.")
+        userInput = input("> ")
+        userInput = userInput.lower().strip()
+    if userInput == 's': 
+            addSong()
+    elif userInput == 'f':
+        topListen()
+    return 
+
+
+
+def startSess():
+
+    connection, cursor = connect(path)
+    
+    q = '''SELECT *
+        FROM sessions'''
+    cursor.execute(q)
+    newSession = len(cursor.fetchall()) + 1
+    connection.commit()
+
+    q = '''
+    INSERT INTO sessions(sno, start, end)
+    VALUES(?, datetime('now'), NULL)
+    '''
+    cursor.execute(q, (newSession,))
+    connection.commit()
+
+    return newSession 
+
+def endSess(sessNo):
+
+    connection, cursor = connect(path)
+    q = '''INSERT INTO sessions(end)
+    VALUES(datetime('now'))
+    WHERE sessions.sno = ?'''
+    cursor.execute(q, (sessNo))
+    connection.commit()
+
+    return 
+    
+def orderByKW(arr, keys):
+    #arr is the tuple, keys is the keyword, order DESC with the top
+    #most array being the one with the most key words matched
+    return
+
+
+def songInfo():
+    #get artist name, sid, title and duration + any playlist the song is in
+    connection, cursor = connect(path)
+    q = '''SELECT
+    FROM
+    WHERE'''
+    cursor.execute(q)
+    songInfo = cursor.fetchone()
+    connection.commit()
+
+    for info in songInfo: 
+        print(info)
+
+    return
+
+def user(user):
+    #user is an uid of the user to logged in
+    connection, cursor = connect(path)
+
+
+    print("To start a session enter 'S'\n To search for a song or playlist enter 'K'\nEnter 'A' to search for an artist\nTo end the session press 'E': ")
+    userInput = input("> ")
+    userInput = userInput.lower().strip()
+    
+    if userInput == 's':
+        sessNo = startSess()
+    elif userInput == 'k':
+        #" user should be able to provide one or more unique keywords,"
+        # FOCUS ON: Either having it be one input split into an array or requesting multiple inputs for keywords (probably the best????)
+        #must indicate if playlist or song is displayed
+
+        userInput = input("Please enter keywords to search for playlists or songs by spaces only.\n>")
+        
+        #get the keywords into an array
+        keyWords = userInput.split()
+        
+        #get all matching rows from keywords
+        q = ''' 
+
+        SELECT 
+        FROM 
+        WHERE
+
+        '''
+        cursor.execute(q)
+        allMatching = cursor.fetchall()
+        connection.commit()
+
+      
+        '''FIRST: order the tuples by what has the most keywords ***CREATE A FUNCTION FOR THIS WE'LL NEED TO USE IT FOR SEARCHING ARTISTS TOO***
+        SECOND: print the 5 out
+        THIRD: ask what the user wants to do'''
+        orderByKW(allMatching, keyWords)
+        
+        #print the first 5
+        index = 0
+        while index < 5:
+            print(allMatching[index])
+            index += 1 
+        
+        '''one thing to note is that neither song or playlist has a letter to distinguish itself as an id
+                i.e. sid of Knaan would be 1 and not s1
+        ways to solve this: maybe look into finding a way within the for loop to attach an s or a p to the id?
+        only playlists have users, and only song have duration, look into that or other ways 
+       '''
+        print("Enter the id of a playlist or song you want to select as (playlist/song [number])\nEnter 'N' to go to the next 5\Hit 'ENTER' to leave")
+        userInput = input("> ")
+        userInput = userInput.lower().strip()
+        
+        while(userInput[0] != ''):
+            #leave
+            if userInput[0] == '':
+                return
+            
+            #focus on getting song1 to see what the user inputs is a song or a playlist
+            elif userInput[0] == 'song' and int(userInput[1]) > 0:
+                print("Enter 'I' for the song information\nEnter 'L' to listen to the song\nEnter 'A' to add to a playlist\nHit ENTER to leave the selected song")
+                uInput = input("> ")
+                uInput = uInput.lower().strip()
+                
+                #set up a while loop here
+                if uInput == 'i':
+                   songInfo()
+                elif uInput == 'L':
+                    '''a listening event is recorded within the current session of the user (if a session has already started for the user) or within a new session (if not). 
+                    When starting a new session, follow the steps given for starting a session. A listening event is recorded by either inserting a row to table listen or increasing the listen count in this table by 1''' 
+                    print('fkn do something')
+                elif uInput == 'a':
+                    '''When adding a song to a playlist, the song can be added to an existing playlist owned by the user (if any) or to a new playlist.
+                    When it is added to a new playlist, a new playlist should be created with a unique id (created by your system) and the uid set to the id of the user and a title should be obtained from input. '''
+                    print('fkn do something')
+                elif uInput == '':
+                    break
+                else:
+                    print("Invalid input. Try again.")
+
+                
+            elif userInput[0] == 'playlist' and int(userInput[1]) > 0:
+                
+                q = '''SELECT s.sid, s.title, s.duration
+                FROM playlists as p, songs as s, plinclude as pl
+                WHERE  p.pid = pl.pid AND songs.sid = pl.sid
+                '''
+                cursor.execute(q)
+                pSongs = cursor.fetcahll()
+                connection.commit()
+                
+                for song in pSongs:
+                    print(song)
+            elif userInput[0] == 'd':
+                #while index hasn't reached the end or over the array
+                #print the next 5 and ask again
+                while 1:
+                    break
+
+
+    elif userInput == 'a': 
+        userInput = input("Please enter keywords to search for an artist by spaces only.\n>")
+
+        keyWords = userInput.split()
+        q = ''' 
+
+        SELECT 
+        FROM 
+        WHERE
+
+        '''
+        cursor.execute(q)
+        allMatching = cursor.fetchall()
+        connection.commit()
+
+        orderByKW(allMatching, keyWords)
+
+    elif userInput == 'e':
+       endSess(sessNo)
+
+    return
+def main():
+    print("291 Mini-Project 1\n")
+    #todo please check i spelled your names right lmao
+    print("By Anya Hanatuke, Alinn Martinez, and Ayaan Jutt\n")
+    #todo link database using URL
     global path
     connection, cursor = connect(path)
-    register()
-    logReg = input("Would you like to login or register [L/R]? ")
-    while logReg != "l" or logReg != "r":
-        print("Incorrect input. Please try again.")
-        logReg = input("Would you like to login or register [L/R]? ")
-        print(logReg)
-    if logReg == 'r':
-        register()
-    else: 
-        login()
-    connection.close()
+    quit = False
+    while ~quit:
+        initialDone = False
+        while ~initialDone and ~quit:
+            logReg = introLoop()
+            if logReg == 'r':
+                register()
+                initialDone = True
+            elif logReg == 'l':
+                login(cursor)
+                initialDone = True
+            elif logReg == 'q':
+                quit = True
+                print("Thank you.")
+                return
+
+        sessionDone = False
+        while ~sessionDone and ~quit:
+            #todo uh oh this is the hard part
+            break
 main()
